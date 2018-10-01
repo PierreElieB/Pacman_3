25f8d82c38d3f6cf81468769018a2a72d1f0c1a9
PierreElieB <pierre.elie.b@gmail.com>
2018-09-30 21:56:33 -0700

    20 out of 25

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -100,11 +100,15 @@ class QLearningAgent(ReinforcementAgent):
         """
         # Pick Action
         legalActions = self.getLegalActions(state)
-        action = None
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
 
-        return action
+        if(len(legalActions) == 0):
+            return None
+
+        if(util.flipCoin(self.epsilon)):
+            return random.choice(legalActions)
+
+        return self.computeActionFromQValues(state)
+
 
     def update(self, state, action, nextState, reward):
         """
